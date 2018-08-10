76f6a6ce855aeeec2549b5a07369864ff99ee091
hanweiling <hanweiling@primeledger.cn>
2018-08-10 15:53:40 +0800

    #init commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by hanson on 2018/8/10.
+//
+
+#include <libff/common/default_types/ec_pp.hpp>
+#include <libff/algebra/curves/bn128/bn128_pp.hpp>
+#include <libsnark/common/default_types/r1cs_gg_ppzksnark_pp.hpp>
+#include <libsnark/relations/constraint_satisfaction_problems/r1cs/examples/r1cs_examples.hpp>
+#include <libsnark/zk_proof_systems/ppzksnark/r1cs_gg_ppzksnark/r1cs_gg_ppzksnark.hpp>
+
+using namespace libsnark;
+
+int main(){
+    printf("hello world!");
+    return 0;
+}
