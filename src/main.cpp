9a7bb5067800653fbeb5da5447b03af7fce94909
hanweiling <hanweiling@primeledger.cn>
2018-08-10 16:05:03 +0800

    #init commit

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,14 @@
 #include <libsnark/common/default_types/r1cs_gg_ppzksnark_pp.hpp>
 #include <libsnark/relations/constraint_satisfaction_problems/r1cs/examples/r1cs_examples.hpp>
 #include <libsnark/zk_proof_systems/ppzksnark/r1cs_gg_ppzksnark/r1cs_gg_ppzksnark.hpp>
+#include "Address.h"
 
-using namespace libsnark;
+using namespace libzerocash;
 
 int main(){
+    Address address;
+    PublicAddress publicAddress = address.getPublicAddress();
+
     printf("hello world!");
     return 0;
 }
